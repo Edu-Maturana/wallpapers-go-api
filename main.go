bb5a5cae9e55e01423c971118eafe0e6e8096481
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-19 01:24:44 -0300

    add connection function

--- main.go
+++ main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"net/http"
+	"wallpapers/database"
 
 	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	app := echo.New()
+	database.Connect()
 	app.GET("/", func(ctx echo.Context) error {
 		return ctx.String(http.StatusOK, "Hello, World!")
 	})
-	app.Logger.Fatal(app.Start(":1323"))
+	app.Start(":8080")
 }
