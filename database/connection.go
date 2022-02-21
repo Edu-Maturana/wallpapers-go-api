8cb8acefcffbda03e00b9007ff4ece623e98fddb
Eduardo Maturana <edu@Mac-mini-de-Eduardo.local>
2022-02-21 00:02:36 -0300

    database connection done and secure

--- database/connection.go
+++ database/connection.go
@@ -4,17 +4,20 @@ import (
 	"context"
 	"log"
 	"time"
+	"wallpapers/utils"
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var mongoURI = utils.GetEnv("MONGODB_ATLAS_URI")
+
 func Connect() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("to do: add uri"))
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 3*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 1*time.Second)
 	err = client.Connect(ctx)
 	if err != nil {
 		log.Fatal("Error connecting to database", err)
